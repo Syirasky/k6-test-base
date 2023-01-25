eb2889a0cd3de0e7a3a495c1c61c19902f2aa5d0
Rasrizal <rasrizalrosdi96@gmail.com>
2023-01-25 12:47:27 +0800

    add specific scenario execution

--- src/index.js
+++ src/index.js
@@ -1,26 +1,34 @@
 import * as scenario_one from "./test-scenario/scenario-one.js";
 import * as scenario_two from "./test-scenario/scenario-two.js";
 
-// export const options = config.default.options;
-export const options = {
-  scenarios:{
-    scenario_one:{
-      executor: 'per-vu-iterations',
-      exec: 'exec_scenario_one',
-      vus: 1,
-      iterations: 1,
-      maxDuration: '10s',
-      tags: { test_type : 'api' }
-    },
-    scenario_two:{
-      executor: 'per-vu-iterations',
-      exec: 'exec_scenario_two',
-      vus: 1,
-      iterations: 1,
-      maxDuration: '10s',
-      tags: { test_type : 'api' }
-    }
+
+// this index.js contains multiple scenario. 
+// to run a specific scenario k6 run --env scenario=scenario_one
+// but since we using webpack to pack all the js we need to run k6 run --env scenario=scenario_one dist/test.main.js
+
+// specify all available scenarios
+let scenarios_list = {
+  scenario_one:{
+    executor: 'per-vu-iterations',
+    exec: 'exec_scenario_one',
+    vus: 1,
+    iterations: 1,
+    maxDuration: '10s',
+    tags: { test_type : 'api' }
   },
+  scenario_two:{
+    executor: 'per-vu-iterations',
+    exec: 'exec_scenario_two',
+    vus: 1,
+    iterations: 1,
+    maxDuration: '10s',
+    tags: { test_type : 'api' }
+  }
+};
+
+// specify the thresholds here based on the tag
+export const options = {
+  scenarios: {},
   thresholds: {
     'failed form submits{test_type:api}': ['rate<0.1'],
     'failed form fetches{test_type:api}': ['rate<0.1'],
@@ -28,12 +36,21 @@ export const options = {
   }
 };
 
+// functions for the scenarios
 export function exec_scenario_one() {
   const response = scenario_one.test_scenario_one("hello","world");
   console.log(response);
 }
-
 export function exec_scenario_two() {
   const response = scenario_two.test_scenario_two();
   console.log(response);
+}
+
+// this part where the scenario is selected . do not modify.
+if (__ENV.scenario) {
+  // Use just a single scenario if `--env scenario=whatever` is used
+  options.scenarios[__ENV.scenario] = scenarios_list[__ENV.scenario];
+} else {
+  // Use all scenarios
+  options.scenarios = scenarios_list;
 }
